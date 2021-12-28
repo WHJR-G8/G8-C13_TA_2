bbf23b7558721365ad4259f67042b129fcc769ba
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-12-28 11:25:52 +0530

    Update Sol_TA_2.py

--- Sol_TA_2.py
+++ Sol_TA_2.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 Bowler_Height = [160,165,166,168,169,170,174,175,176,177,180,182,183,185,190]
-Bowler_Speed = [80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155]
+Bowler_Speed = [80,85,90,95,100,105,110,115,120,125,130,135,140,145,150]
 
 plt.xlabel("Bowler Height in cm")
 plt.ylabel("Bowler Speed in km/hr")
